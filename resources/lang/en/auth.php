7b6c851ddfc79f8e7b494521133d98734424d4fa
dritonredenica <driton.redenica@gmail.com>
2020-02-09 23:42:56 -0800

    first commit

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Keni gabuar ju lutem provoni perseri',
+    'throttle' => 'Shume tentime per tu kyqur, ju lutem provoni me vone',
+
+];
